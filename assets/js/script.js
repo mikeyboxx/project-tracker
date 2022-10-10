8df1699ee37484fd7fd492c0d0a77c980ca4dc84
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-09 22:09:04 -0400

    Added initialize and start functions. Also changed renderProjectTable function to accept the project list array as an argument.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 function getCurrentDateFormatted(){
-    let dt = moment().format('MMM DD, YYYY !t hh:mm:ss a').replace('!', 'a');  // moment replaces any 'a' chars with am/pm. fake out
-    return dt;
+    return moment().format('MMM DD, YYYY !t hh:mm:ss a').replace('!', 'a');  // moment replaces any 'a' chars with am/pm. fake out
 }
 
 function renderCurrentDateTime(){
-    $(formattedDateTime).text(getCurrentDateFormatted());
-    let timer = setInterval(()=> $(formattedDateTime).text(getCurrentDateFormatted()), 1000);
+    $('#formattedDateTime').text(getCurrentDateFormatted());
+    let timer = setInterval(()=> $('#formattedDateTime').text(getCurrentDateFormatted()), 1000);
 }
-
 
 function getFormData(){
     return {
@@ -18,72 +16,75 @@ function getFormData(){
     }
 }
 
-
-function renderProjectTable(){
-    $('#tblProjectList tbody').innerHTML = '';
+function renderProjectTable(arrProjectList){
+    $('#tblProjectList tbody').empty();
 
     for(let i=0; i<arrProjectList.length;i++){
+        let projDayOfYear = moment(arrProjectList[i].dueDate).dayOfYear();
+        let currDayOfYear = moment().dayOfYear();
+
+        projDayOfYear = (projDayOfYear < currDayOfYear) ? projDayOfYear+=365 : projDayOfYear;
+        let diffDays = projDayOfYear - currDayOfYear;
+
         $('#tblProjectList tbody')
-            .append($('<tr>')
-                .append(
-                    $('<td>').text(arrProjectList[i].name)
-                )
-                .append(
-                    $('<td>').text(arrProjectList[i].type)
-                )
-                .append(
-                    $('<td>').text(arrProjectList[i].rate)
-                )
-                .append(
-                    $('<td>').text(arrProjectList[i].dueDate)
-                )
-                .append(
-                    $('<td>').text(moment(arrProjectList[i].dueDate).dayOfYear() - moment().dayOfYear())
-                )
-                .append(
-                    $('<td>').text(arrProjectList[i].rate * 8 * (moment(arrProjectList[i].dueDate).dayOfYear() - moment().dayOfYear()))
-                )
-                .append(
-                    $('<td>')
-                        .append('<button>')
+            .append($('<tr>').attr('id',`${i}`)
+                .append($('<td>').text(arrProjectList[i].name))
+                .append($('<td>').text(arrProjectList[i].type))
+                .append($('<td>').text(arrProjectList[i].rate))
+                .append($('<td>').text(arrProjectList[i].dueDate))
+                .append($('<td>').text(diffDays))
+                .append($('<td>').text(arrProjectList[i].rate * 8 * diffDays))
+                .append($('<td>')
+                    .append($('<button>')
                         .addClass('btn btn-link')
                         .text('X')
+                        .click((event)=>{
+                            event.stopPropagation();
+                            let trId = $(event.target).parent().parent()[0].id;
+                            arrProjectList.splice(trId, 1);
+                            localStorage.setItem('projectList', JSON.stringify(arrProjectList));
+                            renderProjectTable(arrProjectList);
+                        })
+                    )
                 )
             );
     }
-
 }
 
-
-
-
-
-
-var arrProjectList = JSON.parse(localStorage.getItem('projectList'));
-
-if (arrProjectList===null) arrProjectList = [];
-
-renderProjectTable();
-renderCurrentDateTime();
-
-
-
-
-$('#formSubmitProject').on('submit', (event)=>{
-    event.preventDefault();
-
-    arrProjectList.push(getFormData());
-    localStorage.setItem('projectList', JSON.stringify(arrProjectList));
-
-    $('#staticBackdrop').modal('hide');
-    $('#projectName').val(''),
-    $('#projectType').val('');
-    $('#hourlyRate').val(''),
-    $('#dueDate').val(''),
+function initialize(arrProjectList){
+    arrProjectList = JSON.parse(localStorage.getItem('projectList'));
+    if (arrProjectList===null) arrProjectList = [];
     
-    console.log(arrProjectList);
-    renderProjectTable();
-})
+    $('#formSubmitProject').on('submit', (event)=>{
+        event.preventDefault();
+        
+        arrProjectList.push(getFormData());
+        localStorage.setItem('projectList', JSON.stringify(arrProjectList));
+        
+        $('#staticBackdrop').modal('hide');
+        $('#projectName').val(''),
+        $('#projectType').val('');
+        $('#hourlyRate').val(''),
+        $('#dueDate').val(''),
+        
+        renderProjectTable(arrProjectList);
+    });
+}
+
+function start(){
+    var arrProjectList = [];
+
+    initialize(arrProjectList);
+    renderCurrentDateTime();
+    renderProjectTable(arrProjectList);
+}
+
+start();
+
+
+
+
+
 
 
 // console.log(getCurrentDateFormatted());
